4c20c5482ba9d151813ed969b04cb0be50130dad
Muhammad Fatih <muhammadfatih4994@gmail.com>
2021-06-14 14:24:23 +0700

    kasih middlewere

---
+++ CheckAuth.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const {OAuth2Client} = require('google-auth-library');
+const clientAndroid = new OAuth2Client(process.env.CLIENT_ID_ANDROID);
+const clientIOS = new OAuth2Client(process.env.CLIENT_ID_IOS);
+
+
+async function CheckAuth(req, res, next) {
+    const {platform} = req.body;
+    const { authorization } = req.headers;
+    if (!authorization) return res.sendStatus(400);
+    const token = authorization.split(" ")[1];
+    if(!token) return res.sendStatus(400);
+    const client = platform === "ANDROID"?clientAndroid : clientIOS
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: [CLIENT_ID_ANDROID,CLIENT_ID_IOS],
+    }).catch(err => {
+        console.log("INVALID TOKEN : ", err);
+        return null
+    });
+    if(!ticket) return res.sendStatus(403);
+    const payload = ticket.getPayload();
+    // If request specified a G Suite domain:
+    // const domain = payload['hd'];
+    req.payload = payload
+    next();
+}
+
+exports.CheckAuth = CheckAuth;
--- index.js
+++ index.js
@@ -1,37 +1,17 @@
+require('dotenv').config();
 const express = require('express')
 const mongoose = require('mongoose')
 const app = express()
 const Laporan = require('./models/laporan')
-const {OAuth2Client} = require('google-auth-library');
-const clientAndroid = new OAuth2Client(process.env.CLIENT_ID_ANDROID);
-const clientIOS = new OAuth2Client(process.env.CLIENT_ID_IOS);
-require('dotenv').config();
 app.use(express.json()) // for parsing application/json
 app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
-
-const mongoDB = process.env.MONGO_URI
+const {CheckAuth} = require('./CheckAuth');
+const mongoDB = process.env.MONGO_URI ?? ""
 mongoose.connect(mongoDB, {useNewUrlParser: true, useUnifiedTopology: true})
 .then((res)=>{
     console.log(res)
     app.listen(process.env.PORT)
 })
-
-async function verify(token,platform) {
-    const client = platform === "ANDROID"?clientAndroid : clientIOS
-    const ticket = await client.verifyIdToken({
-        idToken: token,
-        audience: [CLIENT_ID_ANDROID,CLIENT_ID_IOS],  // Specify the CLIENT_ID of the app that accesses the backend
-        // Or, if multiple clients access the backend:
-        //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
-    }).catch(err => {
-        console.log("INVALID TOKEN");
-    });
-    if(!ticket) return false;
-    const payload = ticket.getPayload();
-    // If request specified a G Suite domain:
-    // const domain = payload['hd'];
-    return payload
-  }
 
 // app.get('/token', async (req,res)=>{
 //     const token = "eyJhbGciOiJSUzI1NiIsImtpZCI6IjZhOGJhNTY1MmE3MDQ0MTIxZDRmZWRhYzhmMTRkMTRjNTRlNDg5NWIiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL2FjY291bnRzLmdvb2dsZS5jb20iLCJhenAiOiI4NjQ4ODg5MDk4ODItNTJkZzU4MWozbWFjMGw1b2lndXRtZnNjbmRwZGM5NWEuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJhdWQiOiI4NjQ4ODg5MDk4ODItNTJkZzU4MWozbWFjMGw1b2lndXRtZnNjbmRwZGM5NWEuYXBwcy5nb29nbGV1c2VyY29udGVudC5jb20iLCJzdWIiOiIxMDAwMTU1Nzc0NTE0OTIwNzM5OTYiLCJlbWFpbCI6InJiaW50YW5nLmJhZ3VzMTFAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWUsImF0X2hhc2giOiJwWmhUcTFPaHB5ZWlXbk5aSUxjZUNRIiwibm9uY2UiOiJlWFJhYUdLbXFrNWhTT2w0cVFFNnZKR01SZTF6TWJ4aDBaRy1jNFdvdjJBIiwibmFtZSI6ImVyIGJpbnQiLCJwaWN0dXJlIjoiaHR0cHM6Ly9saDUuZ29vZ2xldXNlcmNvbnRlbnQuY29tLy1aNmllUnlOdWJJby9BQUFBQUFBQUFBSS9BQUFBQUFBQUFBQS9BTVp1dWNuSmZjNGZPcHFmUWhRVS12cFoxMDJzeU9WNXJRL3M5Ni1jL3Bob3RvLmpwZyIsImdpdmVuX25hbWUiOiJlciIsImZhbWlseV9uYW1lIjoiYmludCIsImxvY2FsZSI6ImlkIiwiaWF0IjoxNjE2NzczMDc1LCJleHAiOjE2MTY3NzY2NzV9.iVo9vliAu2dBuPtQLvitAjCWUC2RLAM8cTkP7ilymDj7hkUC4WMh9b-GEJvQcTni8BuM4FM4YqlDywGvrFl3MMzI8-1QZHY_8NHrX1MwVB0pArx_9VzIeNU-dXFt6i77VnupyrG5AinXOh32yOrjl8dlV9x4HHfYaWsTSHBbTTQU7PHNa5aNxrMcabCUO3uQssRXUrNjEAsSDRoLNrPNKLRuUn804smVGdOkSUwg5bjKhSfxqwCGa1ZEnPTJgWk-pocHSlr0oWeaL5ZVecIy4cF_bEg9GcV0WAaPNDP5J-EW6bSQUDhTJU3gi84BcUgRS1Ka7uUbfAyaE13RCTjTWg";
@@ -46,7 +26,7 @@ app.get('/',(req,res)=>{
 })
 
 //create laporan
-app.post('/buat-laporan', async (req,res) => {
+app.post('/buat-laporan', CheckAuth, async (req,res) => {
     const {
         title, 
         id_laporan, 
@@ -57,12 +37,10 @@ app.post('/buat-laporan', async (req,res) => {
         email_pelapor, 
         status, 
         datetime,
-        token,
         } = req.body
-    const payload = await verify(token,"IOS");
+    
+    const payload = req.payload;
     console.log(payload);
-    if(!payload) return res.status(403).send("INVALID TOKEN");
-    console.log(payload)
     const lapor = new Laporan({
         title, 
         id_laporan, 
@@ -75,6 +53,7 @@ app.post('/buat-laporan', async (req,res) => {
         datetime,
     }
     )
+
     const result = await lapor.save();
     return res.send(result);
 })
